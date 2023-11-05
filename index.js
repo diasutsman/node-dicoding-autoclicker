99556b9b83d8b494a91096d1ac9220ff2b608250
Dias Utsman <utsmand91@gmail.com>
2023-11-05 16:06:49 +0700

    Refactor the code and make some variable fillable from .env

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 require("dotenv").config();
 const puppeteer = require("puppeteer");
 
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function login(page) {
+	await page.goto("https://www.dicoding.com/login");
+	await page.type("input[name=login_email]", process.env.DICODING_USERNAME);
+	await page.type("input[name=login_password]", process.env.DICODING_PASSWORD);
+	await page.click('button[type="submit"]');
+	await page.waitForNavigation();
+}
+
+async function performClicks(page, clickCount = Number.MAX_SAFE_INTEGER) {
+	const buttonSelector = "a.classroom-bottom-nav__next"; // Replace with the selector of the button you want to click
+
+	for (let i = 0; i < clickCount; i++) {
+		console.log(`Proceed to url: ${page.url()}`);
+		console.log(`Press ctrl + C to exit.`);
+		await Promise.all([
+			page.click(buttonSelector),
+			page.waitForNavigation(),
+			sleep(2500),
+		]);
+	}
+}
+
+async function gotoFirstUrl(page) {
+	const dicodingUrl = process.env.START_URL;
+	await page.goto(dicodingUrl);
+}
+
 (async () => {
 	const browser = await puppeteer.launch({
 		headless: false, // Set to true for headless mode
 	});
 	const page = await browser.newPage();
 
-	// Navigate to the login page and log in
-	await page.goto("https://www.dicoding.com/login");
-	await page.type("input[name=login_email]", process.env.DICODING_USERNAME);
-	await page.type("input[name=login_password]", process.env.DICODING_PASSWORD);
-	await page.click('button[type="submit"]');
+	await login(page);
 
-	// Wait for the login to complete
-	await page.waitForNavigation();
+	await gotoFirstUrl(page);
 
-	// Navigate to the target page and perform the desired actions
-	const dicodingUrl = "https://www.dicoding.com/academies/423/tutorials/25845";
-	await page.goto(dicodingUrl);
-
-	// Replace the following steps with your sequence of actions
-	const buttonSelector = "a.classroom-bottom-nav__next"; // Replace with the selector of the button you want to click
-	const numIterations = 1000; // Replace with the number of times you want to repeat the action
-
-	for (let i = 0; i < numIterations; i++) {
-		await Promise.all([page.click(buttonSelector), page.waitForNavigation()]);
-	}
-
-	// Close the browser when you're done
-	await browser.close();
+	await performClicks(page);
 })();
